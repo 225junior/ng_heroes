6c681e107e95641d0e395b9287da5b254c51e97a
225junior <225juniorbrindou@gmail.com>
2023-07-03 15:43:59 +0000

    feat: fin de l'ajout des services et l'interaction service-service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,19 +12,24 @@ export class HeroesComponent {
   heroes :Hero[] = [] ;
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService){}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+    ){}
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void{
     this.heroService.getHeroes().subscribe(data => {
       this.heroes = data;
     });
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
 
+  messages: string[] = [];
   constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 }
